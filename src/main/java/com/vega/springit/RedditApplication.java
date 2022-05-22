c03ce67320fbf9592ca974f86b82b3565df6d7b3
gc <gary.5060@hotmail.com>
2022-05-22 16:01:00 -0400

    Print Welcome Message

--- src/main/java/com/vega/springit/RedditApplication.java
+++ src/main/java/com/vega/springit/RedditApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RedditApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedditApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RedditApplication.class, args);
+        System.out.println("Welcome to sprintit
+    }
 
 }
